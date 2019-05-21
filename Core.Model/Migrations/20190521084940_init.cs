2ac061aca0c7e16999d49216c881cc18de6eca95
nanshan03 <734463238@qq.com>
2019-05-21 16:59:34 +0800

    feat:增加mysql数据库的codefirst引用

---
+++ Core.Model/Migrations/20190521084940_init.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Core.Model.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    nickName = table.Column<string>(nullable: true),
+                    loginAccount = table.Column<string>(nullable: true),
+                    loginPwd = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
